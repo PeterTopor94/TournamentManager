ee079e446ce8c47dfec00fd70e12667b942a0288
Beňo Luboš <lubos.beno@ixperta.com>
2017-10-29 14:17:26 +0100

    ending implementation of BadgeDao

--- Data/src/main/java/cz/muni/fi/pa165/pokemons/dao/BadgeDaoImpl.java
+++ Data/src/main/java/cz/muni/fi/pa165/pokemons/dao/BadgeDaoImpl.java
@@ -6,6 +6,7 @@
 package cz.muni.fi.pa165.pokemons.dao;
 
 import cz.muni.fi.pa165.pokemons.entities.Badge;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
@@ -38,6 +39,16 @@ public class BadgeDaoImpl implements BadgeDao {
     @Override
     public void update(Badge b) {
        
+    }
+
+    @Override
+    public List<Badge> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Badge> findByOwner(Trainer t) {
+        em.createQuery("Select ")
     }
     
     
--- Data/src/main/java/cz/muni/fi/pa165/pokemons/entities/Badge.java
+++ Data/src/main/java/cz/muni/fi/pa165/pokemons/entities/Badge.java
@@ -6,9 +6,11 @@
 package cz.muni.fi.pa165.pokemons.entities;
 
 import java.util.ArrayList;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
@@ -23,13 +25,14 @@ public class Badge {
     private String cityOfOrigin;
     
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     @NotNull
     @Column(nullable = false)
     private Gym gym;
 
+    
     private ArrayList<Trainer> owners;
 
     public Badge(String cityOfOrigin, Gym gym, ArrayList<Trainer> owners) {
@@ -61,4 +64,31 @@ public class Badge {
     public void addOwner(Trainer owner) {
         owners.add(owner);
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.cityOfOrigin);
+        hash = 37 * hash + Objects.hashCode(this.id);
+        hash = 37 * hash + Objects.hashCode(this.gym);
+        hash = 37 * hash + Objects.hashCode(this.owners);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Badge other = (Badge) obj;
+        return true;
+    }
+    
+    
 }
