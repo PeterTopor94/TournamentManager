94799d9113f58d6ae8b79c6f0a63e46705c780bf
Beňo Luboš <lubos.beno@ixperta.com>
2017-10-29 21:53:52 +0100

    added test class

---
+++ Data/src/test/java/GymDaoTest.java
@@ -0,0 +1,24 @@
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lubos.beno
+ */
+@ContextConfiguration
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class GymDaoTest extends AbstractTestNGSpringContextTests {
+    
+}
