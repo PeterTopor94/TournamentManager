504f1d0bb16dc4bdc984d2cd830f6c9f7f171c09
Peter Topor <Peter Topor@DESKTOP-EGVKIGA>
2017-10-30 20:28:46 +0100

    Add imports

--- Data/src/main/java/cz/muni/fi/pa165/pokemons/dao/PokemonDao.java
+++ Data/src/main/java/cz/muni/fi/pa165/pokemons/dao/PokemonDao.java
@@ -1,5 +1,7 @@
 package cz.muni.fi.pa165.pokemons.dao;
 
+import cz.muni.fi.pa165.pokemons.entities.Pokemon;
+import cz.muni.fi.pa165.pokemons.entities.Trainer;
 import java.util.List;
 import cz.muni.fi.pa165.pokemons.enums.PokemonType;
 
