e31d602a618397d50a864b3d2ddc3b88fc1c78d2
Alkanthor <rogls@seznam.cz>
2017-11-22 21:19:33 +0100

    created TrainerDTO

---
+++ API/src/main/java/cz/muni/fi/pa165/pokemons/DTO/TrainerDTO.java
@@ -0,0 +1,117 @@
+package cz.muni.fi.pa165.pokemons.DTO;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author Roman Gluszny
+ */
+public class TrainerDTO {
+    private Long id;
+    
+    private String name;
+    
+    private String surname;
+    
+    private List<BadgeDTO> badges = new ArrayList<>();
+    
+    private List<PokemonDTO> pokemons = new ArrayList<>();
+    
+    private Date dateOfBirth;
+    
+    private GymDTO gym;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public List<BadgeDTO> getBadges() {
+        return badges;
+    }
+
+    public void setBadges(List<BadgeDTO> badges) {
+        this.badges = badges;
+    }
+
+    public List<PokemonDTO> getPokemons() {
+        return pokemons;
+    }
+
+    public void setPokemons(List<PokemonDTO> pokemons) {
+        this.pokemons = pokemons;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public GymDTO getGym() {
+        return gym;
+    }
+
+    public void setGym(GymDTO gym) {
+        this.gym = gym;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode(this.name);
+        hash = 29 * hash + Objects.hashCode(this.surname);
+        hash = 29 * hash + Objects.hashCode(this.dateOfBirth);
+        hash = 29 * hash + Objects.hashCode(this.gym);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TrainerDTO other = (TrainerDTO) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.surname, other.surname)) {
+            return false;
+        }
+        if (!Objects.equals(this.dateOfBirth, other.dateOfBirth)) {
+            return false;
+        }
+        if (!Objects.equals(this.gym, other.gym)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
