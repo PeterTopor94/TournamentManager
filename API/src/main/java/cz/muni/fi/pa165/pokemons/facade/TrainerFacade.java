320a33899a7fb4152e18b87cef852a03afcd1847
Alkanthor <rogls@seznam.cz>
2017-11-22 21:27:13 +0100

    created trainer facade class and interface

---
+++ API/src/main/java/cz/muni/fi/pa165/pokemons/facade/TrainerFacade.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.pokemons.facade;
+
+import cz.muni.fi.pa165.pokemons.DTO.TrainerDTO;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Roman Gluszny
+ */
+public interface TrainerFacade {
+    
+    public List<TrainerDTO> getAllTrainers();
+    public List<TrainerDTO> getTrainersByNameAndSurname(String name, String surname);
+    public List<TrainerDTO> getTrainersByDateOfBirth(Date birthdate);
+    public TrainerDTO getTrainerByGym(Long gymId);
+    public List<TrainerDTO> getAllTrainersForTournament(Long tournamentId);
+    //trainers having pokemon
+    //trainers having badge
+}
---
+++ Service/src/main/java/cz/muni/fi/pa165/pokemons/service/facade/TrainerFacadeImpl.java
@@ -0,0 +1,70 @@
+package cz.muni.fi.pa165.pokemons.service.facade;
+
+import cz.muni.fi.pa165.pokemons.DTO.TrainerDTO;
+import cz.muni.fi.pa165.pokemons.entities.Gym;
+import cz.muni.fi.pa165.pokemons.entities.Tournament;
+import cz.muni.fi.pa165.pokemons.entities.Trainer;
+import org.springframework.beans.factory.annotation.Autowired;
+import cz.muni.fi.pa165.pokemons.facade.TrainerFacade;
+import java.util.Date;
+import java.util.List;
+
+
+/**
+ *
+ * @author Roman Gluszny
+ */
+public class TrainerFacadeImpl implements TrainerFacade {
+
+    @Autowired
+    private TrainerService trainerService;
+
+    @Autowired
+    private BadgeService badgeService;
+
+    @Autowired
+    private PokemonService pokemonService;
+
+    @Autowired
+    private GymService gymService;
+    
+    @Autowired
+    private TournamentService tournamentService;
+
+    @Autowired
+    private BeanMappingService beanMappingService;
+
+    @Override
+    public List<TrainerDTO> getAllTrainers() {
+        return beanMappingService.mapTo(trainerService.findAllTrainers(),
+                TrainerDTO.class);
+    }
+
+    @Override
+    public List<TrainerDTO> getTrainersByNameAndSurname(String name, String surname) {
+        return beanMappingService.mapTo(trainerService.findByNameAndSurname(name, surname),
+                TrainerDTO.class);
+    }
+
+    @Override
+    public List<TrainerDTO> getTrainersByDateOfBirth(Date birthdate) {
+        return beanMappingService.mapTo(trainerService.getTrainersByDateOfBirth(birthdate),
+                TrainerDTO.class);
+    }
+
+    @Override
+    public TrainerDTO getTrainerByGym(Long gymId) {
+        Gym gym = gymService.findGymById(gymId);
+       Trainer trainer = trainerService.getTrainerByGym(gym);
+        return (trainer == null) ? null : beanMappingService.mapTo(trainer, TrainerDTO.class);
+    }
+
+    @Override
+    public List<TrainerDTO> getAllTrainersForTournament(Long tournamentId) {
+        Tournament tournament = tournamentService.findGymById(tournamentId);
+        List<Trainer> trainers = trainerService.getAllTrainersForTournament(tournament);
+
+        return beanMappingService.mapTo(trainers, TrainerDTO.class);
+    }
+
+}
