a2ab3499e46a9649a15facfb3c7049e61244edc9
Miroslav <Miroslav@Miroslav>
2017-11-26 13:39:30 +0100

    new class TournamentDTO is created

--- API/src/main/java/cz/muni/fi/pa165/pokemons/DTO/TournamentDTO.java
+++ API/src/main/java/cz/muni/fi/pa165/pokemons/DTO/TournamentDTO.java
@@ -5,10 +5,64 @@
  */
 package cz.muni.fi.pa165.pokemons.DTO;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Miroslav
  */
 public class TournamentDTO {
     
+    private Long id;
+    private String name;
+    private int numRequiredBadges;
+    private List<TrainerDTO> trainers = new ArrayList<TrainerDTO>();
+    
+   public Long getId()
+   {
+       return id;
+   } 
+    
+   public void setId(Long id){
+       this.id = id;
+   }
+   
+    private boolean verifyTrainer(TrainerDTO trainer){
+    
+    return (trainer.getBadges().size() >= numRequiredBadges);
+}    
+    
+    public void addTrainer(TrainerDTO t){
+    if (verifyTrainer(t)){
+        trainers.add(t);
+    }
+}
+    
+    public void removeTrainer(TrainerDTO t)
+    {
+        trainers.remove(t);
+    }    
+
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getNumRequiredBadges()
+    {
+        return numRequiredBadges;
+    }
+
+    public void setNumRequiredBadges(int numRequiredBadges)
+    {
+        this.numRequiredBadges = numRequiredBadges;
+    }
+   
 }
