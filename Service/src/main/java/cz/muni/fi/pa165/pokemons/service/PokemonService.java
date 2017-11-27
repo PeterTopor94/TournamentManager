6909f31fbe820517f71a3640d34ae6dcb01f9617
Peter Topor <Peter Topor@DESKTOP-EGVKIGA>
2017-11-27 20:25:25 +0100

    Create service layer for pokemon

--- Service/src/main/java/cz/muni/fi/pa165/pokemons/service/PokemonService.java
+++ Service/src/main/java/cz/muni/fi/pa165/pokemons/service/PokemonService.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cz.muni.fi.pa165.pokemons.service;
+
+import java.util.List;
+import cz.muni.fi.pa165.pokemons.entities.Pokemon;
+import cz.muni.fi.pa165.pokemons.entities.Trainer;
+import cz.muni.fi.pa165.pokemons.enums.PokemonType;
 
 /**
  *
  * @author Peter Topor
  */
-public class PokemonService {
+public interface PokemonService {
+    	public Pokemon findById(Long id);
+	public List<Pokemon> findAll();
+	public Pokemon createPokemon(Pokemon p);
+	public void setOwner(Pokemon pokemon, Trainer trainer);
+        public void setLevel(Pokemon pokemon, int level);
+        public void setName(Pokemon pokemon, String name);
+        public void setNickname(Pokemon pokemon, String nickname);
+        public void setPokemonType(Pokemon pokemon, PokemonType pokemonType);
+	public void deletePokemon(Pokemon p);
     
 }
