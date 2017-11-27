9497b0e8a1e1e75103cca9f77ac322bde209973d
Alkanthor <rogls@seznam.cz>
2017-11-27 14:44:17 +0100

    fixed some imports, placed tests into correct package

--- Service/src/test/java/serviceBusinessFunctionTests/TrainerServiceTest.java
+++ Service/src/test/java/serviceBusinessFunctionTests/TrainerServiceTest.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package romanFacadeTest;
+package serviceBusinessFunctionTests;
 
 import cz.muni.fi.pa165.pokemons.dao.TrainerDao;
 import cz.muni.fi.pa165.pokemons.entities.Badge;
