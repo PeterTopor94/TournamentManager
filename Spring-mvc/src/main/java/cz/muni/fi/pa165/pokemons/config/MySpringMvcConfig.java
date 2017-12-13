978a7e06b0501a5defe37b45ffdd07b0172e402a
Matus <matus.krska@gmail.com>
2017-12-13 19:56:07 +0100

    Basic structure of spring-mvc module

--- Spring-mvc/src/main/java/cz/muni/fi/pa165/pokemons/config/MySpringMvcConfig.java
+++ Spring-mvc/src/main/java/cz/muni/fi/pa165/pokemons/config/MySpringMvcConfig.java
@@ -1,5 +1,62 @@
 package cz.muni.fi.pa165.pokemons.config;
 
-public class MySpringMvcConfig
+import cz.muni.fi.pa165.pokemons.service.config.ServiceConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.validation.Validator;
+
+@EnableWebMvc
+@Configuration
+@Import({ServiceConfiguration.class})
+@ComponentScan(basePackages = "cz.muni.fi.pa165.pokemons.controllers")
+public class MySpringMvcConfig extends WebMvcConfigurerAdapter
 {
+
+    /**
+     * Maps the main page to a specific view.
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry)
+    {
+        registry.addViewController("/").setViewName("home");
+    }
+
+
+    /**
+     * Enables default Tomcat servlet that serves static files.
+     */
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer)
+    {
+        configurer.enable();
+    }
+
+    /**
+     * Provides mapping from view names to JSP pages in WEB-INF/jsp directory.
+     */
+    @Bean
+    public ViewResolver viewResolver()
+    {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+
+    @Bean
+    public Validator validator()
+    {
+        return new LocalValidatorFactoryBean();
+    }
 }
